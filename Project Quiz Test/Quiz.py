a8a45799f3fb2ffd05b3c2f96b12fab338f4bb62
Md. Atikur Rahman Tanim <34501560+atiktanim@users.noreply.github.com>
2018-03-14 13:16:36 +0600

    Corrected one

--- Project Quiz Test/Quiz.py
+++ Project Quiz Test/Quiz.py
@@ -28,17 +28,18 @@ for quizNum in range(5):
         del wrongAnswers[wrongAnswers.index(correctAnswer)]
         wrongAnswers = random.sample(wrongAnswers, 3)
         answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
 
        # Write the question and the answer options to the quiz file.
 
-        quizFile.write('%s.What is the famous thing of %s?\n' % (questionNum + 1, zillaName[questionNum])
+        quizFile.write('%s.What is the famous thing of %s?\n' % (questionNum + 1, zillaName[questionNum]))
 
         for i in range(4):
             quizFile.write('    %s. %s\n'%('ABCD'[i],answerOptions[i]))
-     quizFile.write('\n')
+        quizFile.write('\n')
 
     # Write answer key to File
-    answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
     quizFile.close()
     answerKeyFile.close()
 
